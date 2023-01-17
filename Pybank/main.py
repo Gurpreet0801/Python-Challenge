e19115bd41d255842d5e8daea89d36bcc96c8211
Surbhi Bhakoo <dhamejagurpreet@gmail.com>
2023-01-17 14:27:44 -0500

    My First Commit

---
+++ Pybank/main.py
@@ -0,0 +1,50 @@
+#Create and Read file 
+
+import os
+import csv
+
+#Path for the CSV file in Resources
+File = "C:\\Users\\surbh\\Downloads\\Gurpreet\\Gurpreet DA Study\\Module3\\Pybank\\Resources\\budget_data.csv"
+
+#Create the list for sorting
+list_data= []
+
+
+#open the file 
+with open(File) as csvfile:
+    reader = csv.DictReader(csvfile)
+
+    for row in reader:
+        list_data.append({"month": row["Date"], "amount": int(row["Profit/Losses"]),"change": 0})
+
+#Calculating Total Months
+TotalMonths = len(list_data)
+
+#Calculate changes in amount
+Old_amount = list_data[0]["amount"]
+for i in range(TotalMonths):
+    list_data[i]["change"] = list_data[i]["amount"] - Old_amount
+    change_amount = list_data[i]["amount"]
+
+#Calculate total amount
+
+TotalAmount = sum(row["amount"] for row in list_data)
+
+#calculating Average 
+
+TotalChange = sum(row["change"] for row in list_data)
+avg = round(TotalChange/(TotalMonths),2)
+
+#Getting Increase and Decrease 
+#Increase = max(list_data, ["change"])
+#Decrease = min(list_data, ["change"])
+
+#Print the output
+
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {TotalMonths}")
+print(f"Total: ${TotalAmount}")
+print(f"Average Change: ${avg}")
+#print(f'Greatest Increase in Profits: {Increase["month"]} (${Increase["change"]})')
+#print(f'Greatest Decrease in Profits: {Decrease["month"]} (${Decrease["change"]})')
